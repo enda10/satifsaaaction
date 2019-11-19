9aeeef761b4bc95cb402ff20f0aa5a88b6be2662
Enda Healy <enda.healydb10@gmail.com>
2019-11-19 08:24:00 +0100

    remove next flders

--- _next/static/bKvV9zL2JNn9Wmpn9X3aj/pages/home/head.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"4hYy":function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home/head",function(){return o("vc/f")}])}},[["4hYy",0,1]]]);
